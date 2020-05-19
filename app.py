4959293a20e60916ad355f3bd96d0bbd895c5ba8
Leonardo Patiño Rodriguez <leonardo.patino@sophos.col.com>
2020-05-19 11:58:42 -0500

    master

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+PORT= 5000
+DEBUG = False
+
+@app.errorhandler(404)
+def not_found(error):
+	return "Not Found."
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'HOLA LEONARDO'
+
+if __name__ == '__main__':
+	app.run(port = PORT,debug = DEBUG)
